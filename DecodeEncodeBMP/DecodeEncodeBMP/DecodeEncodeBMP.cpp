749850f520906addfb315a9c8e54a0833b60e73d
Attila Bagossy <bagossyattila@outlook.com>
2018-02-09 23:12:55 +0100

    [decode-encode-bmp] Implemented BMP reading

--- DecodeEncodeBMP/DecodeEncodeBMP/DecodeEncodeBMP.cpp
+++ DecodeEncodeBMP/DecodeEncodeBMP/DecodeEncodeBMP.cpp
@@ -3,17 +3,27 @@
 
 #include "stdafx.h"
 #include <GLFW/glfw3.h>
+#include "bmp.h"
 
 int main(void)
 {
+	BitmapFile *bitmapFile;
+	char *data;
+
+	if (load_bitmap_file("C:\\Users\\Attila\\bmp.bmp", &bitmapFile))
+	{
+		return -1;
+	}
+
 	GLFWwindow *window;
 
-	if (!glfwInit()) {
+	if (!glfwInit())
+	{
 		return -1;
 	}
 
 	
-	window = glfwCreateWindow(640, 480, "BMP Viewer", NULL, NULL);
+	window = glfwCreateWindow(bitmapFile->infoHeader.width, bitmapFile->infoHeader.height, "BMP Viewer", NULL, NULL);
 
 	if (!window)
 	{
@@ -27,6 +37,8 @@ int main(void)
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawPixels(bitmapFile->infoHeader.width, bitmapFile->infoHeader.height, GL_BGR_EXT, GL_UNSIGNED_BYTE, bitmapFile->imageData);
 
 		glfwSwapBuffers(window);
 
--- DecodeEncodeBMP/DecodeEncodeBMP/bmp.h
+++ DecodeEncodeBMP/DecodeEncodeBMP/bmp.h
@@ -3,9 +3,62 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <intrin.h>
 
-typedef struct BitmapFileHeader {
-
+typedef struct BitmapFileHeader
+{
+	unsigned short int magic;
+	unsigned int fileSize;
+	unsigned short int garbage[2];
+	unsigned int offset;
 } BitmapFileHeader;
+
+typedef struct BitmapInformationHeader
+{
+	unsigned int headerSize;
+	int width, height;
+	unsigned short int planes;
+	unsigned short int bitsPerPixel;
+	unsigned int compressionMethod;
+	unsigned int imageSize;
+	unsigned int horizontalResolution, verticalResolution;
+	unsigned int colorCount;
+	unsigned int importantColors;
+} BitmapInformationHeader;
+
+typedef struct BitmapFile
+{
+	BitmapFileHeader fileHeader;
+	BitmapInformationHeader infoHeader;
+	void *imageData;
+} BitmapFile;
+
+int load_bitmap_file(char *path, BitmapFile **bitmapFile)
+{
+	FILE *fp;
+	*bitmapFile = nullptr;
+
+	if (fopen_s(&fp, path, "rb")) 
+	{
+		return -1;
+	}
+
+	BitmapFile *filePtr = (BitmapFile *)malloc(sizeof(BitmapFile));
+
+	fread_s(&filePtr->fileHeader, 14, 14, 1, fp);
+
+	fread_s(&filePtr->infoHeader, 40, 40, 1, fp);
+
+	unsigned int rowSize = ((filePtr->infoHeader.bitsPerPixel * filePtr->infoHeader.width + 31) / 32) * 4;
+	unsigned int imageDataSize = rowSize * filePtr->infoHeader.height;
+
+	filePtr->imageData = (char *)malloc(imageDataSize);
+
+	fread_s(filePtr->imageData, imageDataSize, imageDataSize, 1, fp);
+
+	*bitmapFile = filePtr;
+
+	return 0;
+}
 
 #endif // BMP_H
