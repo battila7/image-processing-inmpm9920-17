05b9a9c1a2850bfeedd928b22c40a397c8e8f29f
Attila Bagossy <bagossyattila@outlook.com>
2018-03-11 23:57:04 +0100

    [slow-fourier-transform] Added a low-pass-filter and a progress bar

--- SlowFourierTransform/SlowFourierTransform/image_funcs.h
+++ SlowFourierTransform/SlowFourierTransform/image_funcs.h
@@ -60,19 +60,48 @@ void toComplexImage(unsigned char *data, const int width, const int height, std:
 	{
 		for (long x = 0; x < width; ++x)
 		{
-			result.push_back(std::complex<double>(data[COMPONENT_COUNT * y * width + x * COMPONENT_COUNT], 0));
+			result.push_back(std::complex<double>(data[COMPONENT_COUNT * y * width + x * COMPONENT_COUNT]));
 		}
 	}
 }
 
-void sft(std::vector<std::complex<double>> &data, const int width, const int height, double sign, std::vector<std::complex<double>> &result)
+void butterworthLowPassFilter(std::vector<std::complex<double>> &data, const int width, const int height, double cutoff, double order)
+{
+	for (long y = 0; y < height; ++y)
+	{
+		for (long x = 0; x < width; ++x)
+		{
+			long shiftedY = y - (height / 2);
+			long shiftedX = x - (width / 2);
+
+			double dist = std::sqrt(shiftedY * shiftedY + shiftedX * shiftedX);
+
+			double h = 1.0 / (1.0 + std::pow(dist / cutoff, 2 * order));
+
+			data[width * y + x] *= h;
+		}
+	}
+}
+
+void slowFourierTransform(std::vector<std::complex<double>> &data, const int width, const int height, double sign, std::vector<std::complex<double>> &result)
 {
 	double size = (double)(width * height);
+
+	double step = size / 10.0;
+
+	double percent = 0;
 
 	for (double v = 0; v < height; ++v)
 	{
 		for (double u = 0; u < width; ++u)
 		{
+			double current = v * width + u, intpart;
+
+			if (std::modf((current + width + 1) / step, &intpart) == 0)
+			{
+				printf("  %.0f%%", ((current + width + 1) / step) * 10);
+			}
+
 			auto sum = std::complex<double>(0.0, 0.0);
 
 			for (double y = 0; y < height; ++y)
@@ -83,7 +112,7 @@ void sft(std::vector<std::complex<double>> &data, const int width, const int hei
 					exponent += (u * x) / (double)width;
 					exponent *= sign * 2.0 * M_PI;
 
-					sum += data[y * width + x] * std::exp(std::complex<double>( 1, exponent ));
+					sum += data[y * width + x] * std::exp(std::complex<double>( 0, exponent ));
 				}
 			}
 
@@ -92,6 +121,8 @@ void sft(std::vector<std::complex<double>> &data, const int width, const int hei
 			result.push_back(sum);
 		}
 	}
+
+	printf("\n");
 }
 
 void flipQuadrants(std::vector<std::complex<double>> &data, const int width, const int height)
@@ -156,142 +187,6 @@ void toBrightnessImage(std::vector<double> &data, std::vector<unsigned char> &re
 		result.push_back((unsigned char)data[i]);
 		result.push_back((unsigned char)data[i]);
 		result.push_back((unsigned char)data[i]);
-	}
-}
-
-void slowFourierTransform(unsigned char *data, const int width, const int height, std::vector<unsigned char> &outResult, double sign)
-{
-	using complex = std::complex<double>;
-
-	std::vector<double> norms;
-
-	double size = (double)(width * height);
-
-	for (double v = 0; v < height; ++v)
-	{
-		for (double u = 0; u < width; ++u)
-		{
-			auto sum = complex(0.0, 0.0);
-
-			double re = 0;
-			double im = 0;
-
-			for (double y = 0; y < height; ++y)
-			{
-				for (double x = 0; x < width; ++x)
-				{
-					double exponent = (v * y) / (double)height;
-					exponent += (u * x) / (double)width;
-					exponent *= 2.0 * M_PI;
-
-					double s = data[(long)(3.0 * y * width + x * 3.0)];
-
-					re += (double)s * std::cos(exponent);
-					im -= (double)s * std::sin(exponent);
-
-					// double d = ((double)data[3 * y * width + x * 3]) / 255.0;
-
-					exponent *= sign;
-
-					complex pointAt(s, 0);
-
-					sum += pointAt * std::exp(complex{ 1, exponent });
-				}
-			}
-
-			sum *= 1.0 / std::sqrt(size);
-
-			re *= 1.0 / std::sqrt(size);
-			im *= 1.0 / std::sqrt(size);
-
-			norms.push_back(std::abs(sum));
-			//norms.push_back(std::sqrt(re * re + im * im));
-		}
-	}
-
-	double max = 0, min = 10000000;
-
-	for (long i = 0; i < norms.size(); ++i)
-	{
-	    double res = std::log(1 + norms[i]);
-
-		if (norms[i] < min)
-		{
-			min = norms[i];
-		}
-
-		if (norms[i] > max)
-		{
-			max = norms[i];
-		}
-
-		//norms[i] = res;
-	}
-
-	for (long i = 0; i < norms.size(); ++i)
-	{
-		//double sc = 255.0 / std::log(1 + std::abs(max));
-
-		//unsigned char res = (unsigned char)(sc * std::log(1 + std::abs(norms[i])));
-
-		double sc = (norms[i] - min) / (max - min);
-		unsigned char res = (255.0 / std::log(1 + max)) * std::log(1 + norms[i]);
-
-		outResult.push_back(res);
-		outResult.push_back(res);
-		outResult.push_back(res);
-	}
-
-	/*for (long i = 0; i < norms.size(); ++i)
-	{
-		unsigned char res = 255.0 * ((norms[i] - min) / (max - min));
-		
-		outResult.push_back(res);
-		outResult.push_back(res);
-		outResult.push_back(res);
-	}*/
-}
-
-void fourierShift(unsigned char *data, const int width, const int height)
-{
-	// shift A and C
-	long halfHeight = height / 2, halfWidth = width / 2;
-
-	for (long y = 0; y < halfHeight; ++y)
-	{
-		for (long x = 0; x < halfWidth; ++x)
-		{
-			long posA = 3 * y * width + x * 3;
-			long posC = 3 * (y + halfHeight) * width + (x + halfWidth) * 3;
-
-			unsigned char tmp = data[posC];
-			memset(data + posC, data[posA], 3);
-			memset(data + posA, tmp, 3);
-		}
-	}
-
-	// shift D and B
-	for (long y = 0; y < halfHeight; ++y)
-	{
-		for (long x = halfWidth; x < width; ++x)
-		{
-			long posD = 3 * y * width + x * 3;
-			long posB = 3 * (y + halfHeight) * width + (x - halfWidth) * 3;
-
-			unsigned char tmp = data[posB];
-			memset(data + posB, data[posD], 3);
-			memset(data + posD, tmp, 3);
-		}
-	}
-}
-
-void toImageData(std::vector<double> &in, unsigned char *out)
-{
-	for (int i = 0; i < in.size(); ++i)
-	{
-		out[i * 3] = in[i];
-		out[i * 3 + 1] = in[i];
-		out[i * 3 + 2] = in[i];
 	}
 }
 
